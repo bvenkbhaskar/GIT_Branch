37e675fb7c3de0175afe4a636f2a3086953bfb10
root <root@ip-172-31-26-39.ec2.internal>
2021-08-27 16:02:42 +0000

    New branch Prod-issue created

--- Git_Demo/src/main/java/com/UdaiDemo/app/App.java
+++ Git_Demo/src/main/java/com/UdaiDemo/app/App.java
@@ -1,13 +1,14 @@
 package com.UdaiDemo.app;
 
 /**
- * Hello world!
- *
+ * 
+ * Maven Project for Production-Issue
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Maven project to set branching Stratergy");
+        System.out.println( "This is my first project of maven and branching" );
     }
 }
